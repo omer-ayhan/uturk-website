5238dad85f8e34c1f6305a83611ea4a4948181f3
Ömer Ayhan <om.ayhan247@gmail.com>
2021-09-07 00:50:10 +0300

    added channel list header

--- src/Components/ChannelList.js
+++ src/Components/ChannelList.js
@@ -1,8 +1,83 @@
-import React from "react";
-import Card from "@material-ui/core/Card";
+import React, { useState } from "react";
+import {
+  Card,
+  CardHeader,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+} from "@material-ui/core";
+import Popup from "./Popup";
+import "../css/ChannelList.css";
+import hamburger from "../images/hamburger.png";
+import chevron_down from "../images/chevron-down.png";
+import filter from "../images/filter.png";
 
 function ChannelList() {
-  return <Card>Channels</Card>;
+  const [value, setValue] = useState("inactive");
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    console.log(value);
+  };
+  return (
+    <Card id="channelCard">
+      <CardHeader
+        avatar={<img src={hamburger} alt="" />}
+        action={
+          <div className="categoryMenu">
+            <Popup
+              btn={<img src={filter} alt="" />}
+              out={
+                <List component="nav" aria-label="Live category">
+                  <RadioGroup
+                    aria-label="options"
+                    name="streamFilter"
+                    value={value}
+                    onChange={handleChange}>
+                    <ListItem button>
+                      <FormControlLabel
+                        value="inactive"
+                        control={<Radio />}
+                        label="Başlayan Yayınlar"
+                      />
+                    </ListItem>
+                    <ListItem button>
+                      <FormControlLabel
+                        value="active"
+                        control={<Radio />}
+                        label="Bekleyen Yayınlar"
+                      />
+                    </ListItem>
+                  </RadioGroup>
+                </List>
+              }
+              isButton={false}
+            />
+            <Popup
+              btn={<img src={chevron_down} alt="" />}
+              out={
+                <List component="nav" aria-label="secondary mailbox folders">
+                  <ListItem button>
+                    <ListItemText primary="Basketbol" />
+                  </ListItem>
+                </List>
+              }
+              isButton={false}
+            />
+          </div>
+        }
+        title={
+          <Typography gutterBottom variant="h6" component="h2">
+            Futbol
+          </Typography>
+        }
+      />
+    </Card>
+  );
 }
 
 export default ChannelList;
