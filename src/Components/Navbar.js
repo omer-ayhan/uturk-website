09708ee2886ef7766b3d65923b4574f29886ce4a
Ömer Ayhan <om.ayhan247@gmail.com>
2021-09-06 05:05:55 +0300

    added navbar links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "../css/navbar.css";
-import logo from "../images/uturk.png";
+import logo from "../images/uturk_logo.png";
+
+const nav_links = [
+  { name: "Ana Sayfa", link: "a" },
+  { name: "Yardım", link: "b" },
+  { name: "Hakkımızda", link: "c" },
+];
 
 function Navbar() {
   return (
     <div className="navbar">
       <div id="logo">
-        <a href="#logo">{""}</a>
-        <img className="logo-img" src={logo} alt="logo" />
-        <h1>Utürk</h1>
+        <a href="#logo">
+          <img className="logo-img" src={logo} alt="logo" />
+        </a>
+      </div>
+      <div id="nav-link-group">
+        {nav_links.map(({ name, link }) => (
+          <a className="nav-link" href={`#${link}`}>
+            {name}
+          </a>
+        ))}
       </div>
     </div>
   );
