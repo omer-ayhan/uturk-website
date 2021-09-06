b9ca8e056d65bb8de55d5c903b86ae67fa91788d
Ömer Ayhan <om.ayhan247@gmail.com>
2021-09-06 05:57:34 +0300

    social links added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "../css/navbar.css";
 import logo from "../images/uturk_logo.png";
+import facebook from "../images/facebook.png";
+import twitter from "../images/twitter.png";
+import telegram from "../images/telegram.png";
 
-const nav_links = [
-  { name: "Ana Sayfa", link: "a" },
-  { name: "Yardım", link: "b" },
-  { name: "Hakkımızda", link: "c" },
-];
+const nav_links = {
+  main_links: [
+    { name: "Ana Sayfa", link: "a" },
+    { name: "Yardım", link: "b" },
+    { name: "Hakkımızda", link: "c" },
+  ],
+  social_links: [
+    { img_link: facebook, link: "d" },
+    { img_link: twitter, link: "e" },
+    { img_link: telegram, link: "f" },
+  ],
+};
 
 function Navbar() {
   return (
@@ -17,9 +27,16 @@ function Navbar() {
         </a>
       </div>
       <div id="nav-link-group">
-        {nav_links.map(({ name, link }) => (
+        {nav_links.main_links.map(({ name, link }) => (
           <a className="nav-link" href={`#${link}`}>
             {name}
+          </a>
+        ))}
+      </div>
+      <div id="social-group">
+        {nav_links.social_links.map(({ img_link, link }) => (
+          <a className="social-link" href={`#${link}`}>
+            <img className="social-img" src={img_link} alt="social link" />
           </a>
         ))}
       </div>
