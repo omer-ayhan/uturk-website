6e8c8d721a730b6fcf9dfcc60b77abc2926cbd55
Ömer Ayhan <om.ayhan247@gmail.com>
2021-09-09 17:45:31 +0300

    changes on BackToTop script

--- src/Components/BackToTop.js
+++ src/Components/BackToTop.js
@@ -1,46 +1,28 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { Zoom, Fab, useScrollTrigger } from "@material-ui/core";
 import { images, useStyles } from "./MainStyle";
 
-function ScrollTop(props) {
-  const { children, window } = props;
-  const classes = useStyles();
-  const trigger = useScrollTrigger({
-    target: window ? window() : undefined,
-    disableHysteresis: true,
-    threshold: 100,
-  });
-
+export default function BackToTop() {
   const handleClick = (event) => {
     const anchor = (event.target.ownerDocument || document).querySelector(
       "#back-to-top-anchor"
     );
-    if (anchor) {
-      anchor.scrollIntoView({ behavior: "smooth", block: "center" });
-    }
+    anchor.scrollIntoView({ behavior: "smooth", block: "center" });
   };
+
+  const classes = useStyles();
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
 
   return (
     <Zoom in={trigger}>
       <div onClick={handleClick} role="presentation" className={classes.topBtn}>
-        {children}
+        <Fab color="primary" size="small" aria-label="scroll back to top">
+          <img src={images.arrow_down} alt="" />
+        </Fab>
       </div>
     </Zoom>
-  );
-}
-
-ScrollTop.propTypes = {
-  children: PropTypes.element.isRequired,
-  window: PropTypes.func,
-};
-
-export default function BackToTop(props) {
-  return (
-    <ScrollTop {...props}>
-      <Fab color="primary" size="small" aria-label="scroll back to top">
-        <img src={images.arrow_down} alt="" />
-      </Fab>
-    </ScrollTop>
   );
 }
