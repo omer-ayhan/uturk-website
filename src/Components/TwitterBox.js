0dcffd60cf258c6c5cdd802f914d14d1ece87a8c
Ömer Ayhan <om.ayhan247@gmail.com>
2021-09-21 09:18:07 +0300

    added twitter feed

--- src/Components/TwitterBox.js
+++ src/Components/TwitterBox.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Box, Card, CardHeader, Typography } from "@material-ui/core";
 import MainStyle, { images } from "./MainStyle";
 import { useTranslation } from "react-i18next";
+import { TwitterTimelineEmbed } from "react-twitter-embed";
 
 function TwitterBox() {
   const [useStyles] = MainStyle();
@@ -19,7 +20,13 @@ function TwitterBox() {
           </Typography>
         }
       />
-      <Box height="400px" p="2px 10px"></Box>
+      <Box height="400px" p="2px 10px">
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="uturknet"
+          options={{ height: 400 }}
+        />
+      </Box>
     </Card>
   );
 }
